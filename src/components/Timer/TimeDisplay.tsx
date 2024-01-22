704f91a150228b4df2ab3aa840e509df89792747
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-01-22 10:11:09 -0500

    implement static time display

--- src/components/Timer/TimeDisplay.tsx
+++ src/components/Timer/TimeDisplay.tsx
@@ -1,3 +1,20 @@
 export function TimeDisplay() {
-  return <></>;
+  return (
+    <div className="time-display">
+      <div className="time-unit">
+        <label htmlFor="hours">hr</label>
+        <input type="number" name="hours" defaultValue="00" />
+      </div>
+      <span>:</span>
+      <div className="time-unit">
+        <label htmlFor="minutes">min</label>
+        <input type="number" name="minutes" defaultValue="00" />
+      </div>
+      <span>:</span>
+      <div className="time-unit">
+        <label htmlFor="seconds">sec</label>
+        <input type="number" name="seconds" defaultValue="00" />
+      </div>
+    </div>
+  );
 }
