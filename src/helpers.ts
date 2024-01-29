a71c1b29dd4154a055a9b67e5829b6843c50c97b
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-01-29 14:47:23 -0500

    feat: play sound with notifications (#3)
    * fix: foward URI path to dev server
    
    * feat: play sound on timer completion

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,14 @@
 export function notify() {
   const permissionGranted = window.Notification?.permission === "granted";
-  if (permissionGranted) new Notification("Timer Session Ended");
+  if (permissionGranted) {
+    const notification = new Notification("Timer Session Ended", {
+      silent: false,
+    });
+    notification.addEventListener("show", () => {
+      const audio = new Audio("/success-trumpets.mp3");
+      audio.play().catch(console.error);
+    });
+  }
 }
 
 export function getTodaysEarliestEntryIndex(
