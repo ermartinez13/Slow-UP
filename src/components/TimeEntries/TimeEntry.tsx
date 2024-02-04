6cd11ed194f0abf0c2073373145f85ee9958c65d
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-02-04 11:54:38 -0500

    fix: remove leading and trailing zeroes in dates/time

--- src/components/TimeEntries/TimeEntry.tsx
+++ src/components/TimeEntries/TimeEntry.tsx
@@ -1,6 +1,10 @@
 import { ControlledTextArea } from "../ControlledTextArea";
 import { WorkUnit } from "../Timer/Timer.models";
-import { getTimeSpentStr } from "./TimeEntries.helpers";
+import {
+  getDatesToRender,
+  getTimeSpentToRender,
+  getTimesToRender,
+} from "./TimeEntries.helpers";
 
 interface Props {
   entry: WorkUnit;
@@ -8,29 +12,9 @@ interface Props {
 }
 
 export function TimeEntry({ entry, updateEntry }: Props) {
-  const dateOne = new Date(entry.start);
-  const dateTwo = new Date(entry.end);
-  const dateOneFormatted = `${(dateOne.getMonth() + 1)
-    .toString()
-    .padStart(2, "0")} / ${dateOne.getDate().toString().padStart(2, "0")}`;
-  const dateTwoFormatted = `${(dateTwo.getMonth() + 1)
-    .toString()
-    .padStart(2, "0")} / ${dateTwo.getDate().toString().padStart(2, "0")}`;
-  const timeStart = `${(dateOne.getHours() === 12
-    ? 12
-    : dateOne.getHours() % 12
-  )
-    .toString()
-    .padStart(2, "0")}:${dateOne.getMinutes().toString().padStart(2, "0")}${
-    dateOne.getHours() >= 12 ? "pm" : "am"
-  }`;
-  const timeEnd = `${(dateTwo.getHours() === 12 ? 12 : dateTwo.getHours() % 12)
-    .toString()
-    .padStart(2, "0")}:${dateTwo.getMinutes().toString().padStart(2, "0")}${
-    dateOne.getHours() >= 12 ? "pm" : "am"
-  }`;
-  const timeSpent = entry.spent ? getTimeSpentStr(entry.spent) : "n/a";
-
+  const timeSpent = getTimeSpentToRender(entry.spent);
+  const dates = getDatesToRender(entry.start, entry.end);
+  const times = getTimesToRender(entry.start, entry.end);
   const setContent = (content: string) => {
     updateEntry({ ...entry, description: content });
   };
@@ -44,13 +28,8 @@ export function TimeEntry({ entry, updateEntry }: Props) {
         flexDirection: "column",
       }}
     >
-      <p style={{ marginBlockStart: "0", marginBlockEnd: "0" }}>
-        {dateOneFormatted}{" "}
-        {dateOneFormatted !== dateTwoFormatted ? `- ${dateTwoFormatted}` : null}
-      </p>
-      <p style={{ marginBlockStart: "0", marginBlockEnd: "0" }}>
-        {timeStart} - {timeEnd}
-      </p>
+      <p style={{ marginBlockStart: "0", marginBlockEnd: "0" }}>{dates}</p>
+      <p style={{ marginBlockStart: "0", marginBlockEnd: "0" }}>{times}</p>
       <p style={{ marginBlockStart: "0", marginBlockEnd: "0" }}>{timeSpent}</p>
       <ControlledTextArea
         content={entry.description ? entry.description : entry.text ?? ""}
