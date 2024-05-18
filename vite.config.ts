97e5d38cae462863bb29fc53b78920d3e7aad409
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-05-18 17:36:28 +0000

    fix: build error due to dev certificates vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,22 @@ import react from "@vitejs/plugin-react";
 import fs from "node:fs"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem')
+export default defineConfig(({ command }) => {
+  const config = { plugins: [react()] }
+
+  if (command === "serve") {
+    const devConfig = {
+      server: {
+        host: true,
+        port: 3000,
+        https: {
+          key: fs.readFileSync('./localhost-key.pem'),
+          cert: fs.readFileSync('./localhost.pem')
+        }
+      }
     }
-  },
+    Object.assign(config, devConfig)
+  }
+
+  return config
 });
