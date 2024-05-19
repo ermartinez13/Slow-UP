d247029d914d67f1fa5fb09230f979d47cad4d30
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-05-19 19:35:15 +0000

    refactor: action button to be more readable and DRY

--- src/components/Timer/ActionButtons.tsx
+++ src/components/Timer/ActionButtons.tsx
@@ -8,39 +8,32 @@ interface Props {
 }
 
 export function ActionButtons({ start, pause, stop, status }: Props) {
-  const pauseBtn = (
-    <button onClick={pause} className="w-24 h-6 bg-amber-700">
-      Pause
-    </button>
-  );
-  const onBtn = (
-    <button
-      onClick={() => {
-        if (status === TimerStatus.OFF) {
-          // adds only when transitioning from off -> on
-          window.addEventListener("beforeunload", beforeUnloadHandler);
-        }
-        start();
-      }}
-      className="w-24 h-6 bg-green-700"
-    >
-      {status === TimerStatus.OFF ? "Start" : "Resume"}
+  const isOff = status === TimerStatus.OFF;
+
+  const handleStartPauseClick = () => {
+    if (isOff) {
+      window.addEventListener("beforeunload", beforeUnloadHandler);
+    }
+    isOff ? start() : pause();
+  };
+
+  const handleStopClick = () => {
+    window.removeEventListener("beforeunload", beforeUnloadHandler);
+    stop();
+  };
+
+  const primaryButton = (
+    <button onClick={handleStartPauseClick} className={`${isOff ? "bg-green-700" : "bg-amber-700"} w-24 h-6`}>
+      {isOff ? "Start" : "Pause"}
     </button>
   );
 
   return (
     <div className="flex flex-row justify-center gap-x-4">
-      <button
-        onClick={() => {
-          window.removeEventListener("beforeunload", beforeUnloadHandler);
-          stop();
-        }}
-        disabled={status === TimerStatus.OFF}
-        className="w-24 h-6 bg-zinc-600"
-      >
+      <button onClick={handleStopClick} disabled={isOff} className="w-24 h-6 bg-zinc-600">
         Stop
       </button>
-      {status === "on" ? pauseBtn : onBtn}
+      {primaryButton}
     </div>
   );
 }
