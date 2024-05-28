744e72ad1d082d3fec966f240e2cbeba1d103878
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-05-28 14:42:39 +0000

    fix: time string's slight layout shift

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -7,7 +7,9 @@ interface Props {
 export function Stopwatch({ timeSpent }: Props) {
   return (
     <div className="grid gap-y-8 place-content-center">
-      <h1>{formatDuration(timeSpent, { showTenthsOfASecond: true })}</h1>
+      <h1 className="font-mono">
+        {formatDuration(timeSpent, { showTenthsOfASecond: true })}
+      </h1>
     </div>
   );
 }
