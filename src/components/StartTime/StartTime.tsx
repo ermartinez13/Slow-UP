4383fb04d9bb99eb92672948d7c25f86826a1980
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-05-30 23:03:54 +0000

    refactor: start time rendering logic

--- src/components/StartTime/StartTime.tsx
+++ src/components/StartTime/StartTime.tsx
@@ -5,14 +5,13 @@ interface Props {
 }
 
 export function StartTime({ startTimeMs }: Props) {
-  if (startTimeMs === -1) {
-    return <p>Started at: --</p>;
-  }
-
-  const startTime = formatDateTime(startTimeMs, {
-    showSeconds: true,
-    showAmPm: true,
-  });
+  const startTime =
+    startTimeMs === -1
+      ? "--"
+      : formatDateTime(startTimeMs, {
+          showSeconds: true,
+          showAmPm: true,
+        });
 
   return <p>Started at: {startTime}</p>;
 }
