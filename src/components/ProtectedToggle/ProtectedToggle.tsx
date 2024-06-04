053699ecc3984422856f1d4fab06348122c34846
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-04 07:17:05 -0400

    feat: protected toggling between stopwatch and timer (#19)
    * scaffold protected toggle component
    
    * refactor: protected toggle to be more readable
    
    * wip: use protected toggle in time tracker
    
    * fix: negative time display
    
    * add warning dismissal button
    
    * update warning message and styling
    
    * make warning timeout length a prop

---
+++ src/components/ProtectedToggle/ProtectedToggle.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Toggle } from "../Toggle";
+
+interface Props {
+  isOn: boolean;
+  handleToggle: () => void;
+  offText: string;
+  onText: string;
+  shouldShowWarning: () => boolean;
+  warningMessage: string;
+  warningTimeout?: number;
+}
+
+export function ProtectedToggle({
+  isOn,
+  handleToggle,
+  offText,
+  onText,
+  shouldShowWarning,
+  warningMessage,
+  warningTimeout = 5000,
+}: Props) {
+  const [hasWarned, setHasWarned] = React.useState(false);
+  const [isWarningVisible, setIsWarningVisible] = React.useState(false);
+
+  const handleChange = () => {
+    if (!isOn && !hasWarned && shouldShowWarning()) {
+      setIsWarningVisible(true);
+      setTimeout(() => {
+        setIsWarningVisible(false);
+      }, warningTimeout);
+      setHasWarned(true);
+    } else {
+      handleToggle();
+      setIsWarningVisible(false);
+    }
+  };
+
+  const handleDismissWarning = () => {
+    setIsWarningVisible(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Toggle
+        isOn={isOn}
+        handleToggle={handleChange}
+        offText={offText}
+        onText={onText}
+      />
+      {isWarningVisible && (
+        <div className="bg-yellow-500 dark:bg-yellow-800 p-4 rounded-md text-white dark:text-gray-200 mt-2 max-w-md relative">
+          <button
+            className="absolute top-0 right-0 p-2 text-lg font-bold"
+            onClick={handleDismissWarning}
+          >
+            ×
+          </button>
+          <span className="text-lg">{warningMessage}</span>
+        </div>
+      )}
+    </div>
+  );
+}
