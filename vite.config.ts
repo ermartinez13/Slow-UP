dad49826c9759e2d1c2bae9139c7a2b5c0768097
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-05 18:46:32 +0000

    add alias for source modules

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     test: { environment: "jsdom", setupFiles: "./tests/setup.js" },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   };
 
   if (command === "serve") {
