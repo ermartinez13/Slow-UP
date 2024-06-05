e8175765d15fee7f76efc4a0459ca7352d172708
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-05 13:30:27 +0000

    setup react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "node:fs"
+import fs from "node:fs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const config = { plugins: [react()] }
+  const config = {
+    plugins: [react()],
+    test: { environment: "jsdom", setupFiles: "./tests/setup.js" },
+  };
 
   if (command === "serve") {
     const devConfig = {
@@ -12,13 +16,13 @@ export default defineConfig(({ command }) => {
         host: true,
         port: 3000,
         https: {
-          key: fs.readFileSync('./localhost-key.pem'),
-          cert: fs.readFileSync('./localhost.pem')
-        }
-      }
-    }
-    Object.assign(config, devConfig)
+          key: fs.readFileSync("./localhost-key.pem"),
+          cert: fs.readFileSync("./localhost.pem"),
+        },
+      },
+    };
+    Object.assign(config, devConfig);
   }
 
-  return config
+  return config;
 });
