856fdb21909e51f1fffc6181aba637d2a779b56f
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-06 15:42:47 +0000

    refactor: stopwatch ui to match timer display

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -1,15 +1,24 @@
-import { formatDuration } from "../../helpers";
+import { millisecondsToTimeBreakdown } from "../../helpers";
 
 interface Props {
   timeSpentMs: number;
 }
 
 export function Stopwatch({ timeSpentMs }: Props) {
+  const { hours, minutes, seconds, centiseconds } =
+    millisecondsToTimeBreakdown(timeSpentMs);
+
   return (
-    <div className="grid gap-y-8 place-content-center">
-      <h1 className="font-mono">
-        {formatDuration(timeSpentMs, { showTenthsOfASecond: true })}
-      </h1>
+    <div className="flex justify-center gap-4 h-24">
+      <div className="flex gap-4 text-4xl font-mono self-center">
+        <span>{hours.toString().padStart(2, "0")}</span>
+        <span>:</span>
+        <span>{minutes.toString().padStart(2, "0")}</span>
+        <span>:</span>
+        <span>{seconds.toString().padStart(2, "0")}</span>
+        <span>.</span>
+        <span>{centiseconds.toString().padStart(2, "0")}</span>
+      </div>
     </div>
   );
 }
