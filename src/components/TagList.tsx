3489bc28765c5e8f076a225f9d4c9b0d08da4847
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-18 13:36:31 +0000

    render tags within scrollable area

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,38 +1,45 @@
-import { Button } from "./ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
-interface Props {
+interface TagListProps {
   tags: string[];
   deleteTag: (tag: string) => void;
 }
 
-export function TagList({ tags, deleteTag }: Props) {
+export function TagList({ tags, deleteTag }: TagListProps) {
   return (
-    <div className="space-y-4">
-      {tags.map((tag) => (
-        <div key={tag} className="flex justify-between items-center">
-          <span>{tag}</span>
-          <Button
-            variant="destructive"
-            size="sm"
-            onClick={() => deleteTag(tag)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </Button>
-        </div>
-      ))}
-    </div>
+    <ScrollArea className="h-36 w-full rounded-md border">
+      <div className="p-4">
+        {tags.map((tag, index) => (
+          <div key={tag}>
+            <div className="flex justify-between items-center text-sm">
+              <span>{tag}</span>
+              <Button
+                variant="destructive"
+                size="sm"
+                onClick={() => deleteTag(tag)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-3 h-3"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </Button>
+            </div>
+            {index < tags.length - 1 && <Separator className="my-2" />}
+          </div>
+        ))}
+      </div>
+    </ScrollArea>
   );
 }
