14fc4d6cfa1fad40f07ddc4ccea78bf3bd9e69e2
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-24 11:56:19 +0000

    render add filter ghost button

--- src/components/TimeEntries/TagFilter.tsx
+++ src/components/TimeEntries/TagFilter.tsx
@@ -1,27 +1,41 @@
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Button } from "@/components/ui/button";
 
-interface TagsFilterProps {
+interface Props {
   tags: string[];
   selectedTags: string[];
   setSelectedTags: (tags: string[]) => void;
 }
 
-export function TagsFilter({
-  tags,
-  selectedTags,
-  setSelectedTags,
-}: TagsFilterProps) {
+// export function TagsFilter({
+//   tags,
+//   selectedTags,
+//   setSelectedTags,
+// }: TagsFilterProps) {
+//   return (
+//     <ToggleGroup
+//       type="multiple"
+//       value={selectedTags}
+//       onValueChange={(value) => setSelectedTags(value)}
+//     >
+//       {tags.map((tag) => (
+//         <ToggleGroupItem key={tag} value={tag}>
+//           {tag}
+//         </ToggleGroupItem>
+//       ))}
+//     </ToggleGroup>
+//   );
+// }
+
+export function TagsFilter({ tags, selectedTags, setSelectedTags }: Props) {
   return (
-    <ToggleGroup
-      type="multiple"
-      value={selectedTags}
-      onValueChange={(value) => setSelectedTags(value)}
-    >
-      {tags.map((tag) => (
-        <ToggleGroupItem key={tag} value={tag}>
-          {tag}
-        </ToggleGroupItem>
-      ))}
-    </ToggleGroup>
+    <div>
+      <Button
+        variant="ghost"
+        onClick={() => console.log("clicked add filter button")}
+      >
+        + Add Filter
+      </Button>
+    </div>
   );
 }
