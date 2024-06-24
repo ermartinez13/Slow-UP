ec1e4ec8e7ad88874770812e18440d1cbc62d76a
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-24 12:20:23 +0000

    render uncontrolled select in filter popover

--- src/components/TimeEntries/TagFilter.tsx
+++ src/components/TimeEntries/TagFilter.tsx
@@ -4,6 +4,13 @@ import { Popover, PopoverContent } from "@/components/ui/popover";
 import { PopoverTrigger } from "@radix-ui/react-popover";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface Props {
   tags: string[];
@@ -44,20 +51,18 @@ export function TagsFilter({ tags, selectedTags, setSelectedTags }: Props) {
       </PopoverTrigger>
       <PopoverContent className="w-80">
         <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none">Dimensions</h4>
-            <p className="text-sm text-muted-foreground">
-              Set the dimensions for the layer.
-            </p>
-          </div>
           <div className="grid gap-2">
             <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="width">Width</Label>
-              <Input
-                id="width"
-                defaultValue="100%"
-                className="col-span-2 h-8"
-              />
+              <Label htmlFor="type">Filter Type</Label>
+              <Select defaultValue="OR">
+                <SelectTrigger id="type" className="w-[180px]">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="AND">AND</SelectItem>
+                  <SelectItem value="OR">OR</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
             <div className="grid grid-cols-3 items-center gap-4">
               <Label htmlFor="maxWidth">Max. width</Label>
